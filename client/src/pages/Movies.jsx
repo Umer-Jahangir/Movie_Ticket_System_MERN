8aa2e592e29d1cace4a24cb101e3296c8630b21e
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-08-20 17:30:12 +0500

    Project Completed

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import MovieCard from '../components/MovieCard'
-import BlurCircle from '../components/BlurCircle'
-import { useAppContext } from '../context/AppContext'
-const movies = () => {
+import React from 'react';
+import MovieCard from '../components/MovieCard';
+import BlurCircle from '../components/BlurCircle';
+import { useAppContext } from '../context/AppContext';
 
-  const {shows} = useAppContext();
+const Movies = () => {
+  const { shows } = useAppContext();
 
   return shows.length > 0 ? (
-    <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
-      
-      <BlurCircle top='150px' left='0px' />
-      <BlurCircle bottom='50px' right='50px' />
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+      {/* Blur effects */}
+      <BlurCircle top="150px" left="0px" />
+      <BlurCircle bottom="50px" right="50px" />
 
-      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
-      <div className='flex flex-wrap max-sm:justify-center gap-8'>
-     {shows.map((movie) => (
-       <MovieCard movie = {movie} key={movie._id} />
-     ))}
-    </div>
+      {/* Heading */}
+      <h1 className="text-2xl md:text-3xl font-semibold my-6 text-gray-200">
+        Now Showing
+      </h1>
+
+      {/* Movies Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {shows.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
     </div>
   ) : (
-       <div>
-        <h1 className='text-2xl font-semibold text-gray-300 text-center mt-20'>No Movies Available</h1>
-        <p className='text-gray-400 text-center mt-4'>Please check back later for updates.</p>
-       </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-[70vh] px-6 text-center">
+      <h1 className="text-2xl md:text-3xl font-bold text-gray-300">
+        No Movies Available
+      </h1>
+      <p className="text-gray-400 text-lg mt-2">
+        Please check back later for updates.
+      </p>
+    </div>
+  );
+};
 
-export default movies
+export default Movies;
