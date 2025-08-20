b3141b07109569f7ef6469cf3917b9185063cf4e
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-08-20 14:20:07 +0500

    correct typo

--- server/controllers/stripeWebhooks.js
+++ server/controllers/stripeWebhooks.js
@@ -30,11 +30,11 @@ export const stripeWebhooks = async (request, response)=>{
             })
 
             //send confirmation email
-            await inngest.show({
+            await inngest.send({
               name: "app/show.booked",
               data:{bookingId}
             })
-            
+
             break;
         }
 
